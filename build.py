0ef08b993a71b0f7fcaa4dbd326548741a9b7c37
Marco Ceppi <marco@ceppi.net>
2013-07-13 14:30:38 -0400

    Added build script

---
+++ build.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python
+
+# Use this to build out a new header/footer or when you've updated javascript
+# Don't use this if you've only updated content.
+
+import os
+import re
+import sys
+import glob
+import json
+import time
+
+
+def page_title(page, source=None, itr=None):
+    if not source:
+        with open('htmldocs/navigation.json', 'r') as f:
+            source = json.loads(f.read())
+
+    if 'link' in source:
+        if source['link'] == page:
+            return itr
+
+    for key, val in source.iteritems():
+        if isinstance(val, dict):
+            code = page_title(page, val, key)
+            if code:
+                return code
+
+        if val == page:
+            return key
+
+def fill_tpl(tpl, values={}):
+    for key, val in values.iteritems():
+        tpl = tpl.replace('{{%s}}' % key.upper(), str(val))
+
+    return tpl
+
+if __name__ == '__main__':
+    if not os.path.exists(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'skel')):
+        print "Couldn't find skeleton files"
+        sys.exit(1)
+
+    templates = glob.glob('skel/*.tpl')
+    docs = glob.glob('htmldocs/*.html')
+    timestamp = int(time.time())
+
+
+    for template in templates:
+        with open(template, 'r') as f:
+            tpl = os.path.splitext(os.path.basename(template))[0]
+            tpl_file = f.read()
+            chop = re.compile('<%s>.*?</%s>' % (tpl, tpl), re.DOTALL)
+            for doc in docs:
+                with open(doc, 'r+') as d:
+                    print "%s: %s" % (doc, tpl)
+                    opts = {'PAGE': page_title(os.path.basename(doc)),
+                            'TIMESTAMP': timestamp}
+                    new_html = chop.sub(fill_tpl(tpl_file, opts), d.read())
+                    d.seek(0)
+                    d.write(new_html)
+                    d.truncate()
---
+++ skel/head.tpl
@@ -0,0 +1,16 @@
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <title>Juju Documentation - {{PAGE}}</title>
+  <link href='https://fonts.googleapis.com/css?family=Ubuntu:400,300,300italic,400italic,700,700italic|Ubuntu+Mono' rel='stylesheet' type='text/css' />
+  <link rel="stylesheet" type="text/css" media="screen" href="//juju.ubuntu.com/wp-content/themes/juju-website/css/reset.css" />
+  <link rel="stylesheet" type="text/css" media="screen" href="//juju.ubuntu.com/wp-content/themes/juju-website/css/960.css" />
+  <link rel="stylesheet" type="text/css" media="screen" href="//juju.ubuntu.com/wp-content/themes/juju-website/css/base.css" />
+  <link rel="stylesheet" type="text/css" media="screen" href="//juju.ubuntu.com/wp-content/themes/juju-website/css/home-new.css" />
+  <link rel='stylesheet' id='stacktack-css'  href='//juju.ubuntu.com/wp-content/plugins/stacktack/css/stacktack.min.css?ver=3.4.2' type='text/css' media='all' />
+  <link href="css/main.css?{{TIMESTAMP}}" rel="stylesheet" type="text/css"/>
+
+  <!--[if lt IE 9]>
+  <script type="text/javascript" src="//html5shim.googlecode.com/svn/trunk/html5.js"></script>
+  <![endif]-->
+</head>
+<!-- DO NOT EDIT ABOVE THIS LINE -->
