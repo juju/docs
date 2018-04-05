1b569f12d80aaad545012d3a09777b23ef0d1cb5
Anthony Dillon <me@anthonydillon.com>
2018-04-05 16:57:48 +0100

    Update build docs to respect the new media location

--- build_docs.sh
+++ build_docs.sh
@@ -1,2 +1,2 @@
 rm -rf build
-documentation-builder --source-folder src --tag-manager-code 'GTM-K92JCQ' --search-domain 'docs.jujucharms.com' --search-url 'https://docs.ubuntu.com/search' --search-placeholder 'Search Juju docs' --no-link-extensions
+documentation-builder --source-folder src --media-path src/en/media --output-media-path build/en/media --tag-manager-code 'GTM-K92JCQ' --search-domain 'docs.jujucharms.com' --search-url 'https://docs.ubuntu.com/search' --search-placeholder 'Search Juju docs' --no-link-extensions
